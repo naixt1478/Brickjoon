ae281bfc3fae877c5c67fe7c38ffd468dc9ab890
waixxt3213 <qnqn60360@gmail.com>
2020-10-10 23:20:31 +0900

    solved 10250.cpp

---
+++ 10250/10250.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int a, b, c, n;
+	cin >> n;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a >> b >> c;
+		int f = 100;
+		int e = 1;
+		for (int j = 0; j < c - 1; j++)
+		{
+			if ((j + 1) % a == 0)
+			{
+				f = 100;
+				e++;
+			}
+			else
+			{
+				f += 100;
+			}
+		}
+		cout << e + f << "\n";
+	}
+}

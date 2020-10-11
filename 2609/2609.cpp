7a2c831381ecb8ee4ed6aa53e0cba06da40d263f
waixxt3213 <qnqn60360@gmail.com>
2020-10-11 10:20:36 +0900

    Cleanup All Code

--- 2609/2609.cpp
+++ 2609/2609.cpp
@@ -4,12 +4,12 @@ using namespace std;
 
 int main()
 {
-	int a, b, t, n, n2;
+	int a, b, n, n2;
 	cin >> n >> n2;
 	a = n, b = n2;
 	while (b != 0)
 	{
-		t = a % b;
+		int t = a % b;
 		a = b, b = t;
 	}
 	cout << a << "\n";

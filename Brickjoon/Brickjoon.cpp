802683324f56e6a18fba972b1976d41b334c30e6
naixt1478 <qnqn60360@gmail.com>
2021-08-11 11:07:09 +0900

    pipeline test

--- Brickjoon/Brickjoon.cpp
+++ Brickjoon/Brickjoon.cpp
@@ -6,13 +6,7 @@ using namespace std;
 
 int main()
 {
-	cin.tie(nullptr);
-	ios_base::sync_with_stdio(false);
-	double a,b;
-	cin >> a >> b;
-	a = powf(a, b);
-	cout << a << "\n";
-}
+	zxcvzxcv;
 
 /*
 #include <iostream>

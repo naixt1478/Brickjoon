efe9137ffb0007e5cc4634a5abb8afa44cc51c04
naixt1478 <qnqn60360@gmail.com>
2021-12-28 23:49:38 +0900

    4101.cpp solved

---
+++ Brickjoon_Src/Bronze3/4101.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int a, b;
+    while (cin >> a >> b)
+    {
+        if (!a && !b) break;
+        else if (a > b) cout << "Yes" << '\n';
+        else cout << "No" << '\n';
+    }
+}

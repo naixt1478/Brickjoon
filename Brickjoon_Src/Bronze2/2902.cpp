5d7661a5dec47c7cd0f5e8eb82a4c393d9ac6c5d
naixt1478 <qnqn60360@gmail.com>
2021-12-30 23:24:56 +0900

    2902.cpp solved

---
+++ Brickjoon_Src/Bronze2/2902.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int d, a, b, c;
+    cin >> a >> b >> c;
+    d = min({ a, b, c });
+    if (a + b + c >= 100) cout << "OK";
+    else
+        cout << (a == d ? "Soongsil" : (b == d ? "Korea" : "Hanyang"));
+}

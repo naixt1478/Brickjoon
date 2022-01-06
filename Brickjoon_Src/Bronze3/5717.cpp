1dfeed0e08738de43ef6321803787483f1f83a0b
naixt1478 <qnqn60360@gmail.com>
2022-01-06 23:15:27 +0900

    5717.cpp solved

---
+++ Brickjoon_Src/Bronze3/5717.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	int a, b;
+	while (cin >> a >> b)
+	{
+		if (a == 0 && b == 0) break;
+		cout << a + b << '\n';
+	}
+}
+
+// solve

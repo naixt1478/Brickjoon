693a426ee46792be6a2a225b8e3b0d13e7af14b6
naixt1478 <qnqn60360@gmail.com>
2022-01-13 22:21:34 +0900

    5622.cpp solved

--- Brickjoon_Src/Bronze2/5622.cpp
+++ Brickjoon_Src/Bronze2/5622.cpp
@@ -1,7 +1,30 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
+
+int match(char c)
+{
+	int a = 2, b;
+	b = c - '@';
+	if (b <= 15)
+	{
+		if (b % 3 != 0)a++;
+		return b / 3 + a;
+	}
+	else if (b <= 19) return 6+a;
+	else if (b <= 22) return 7+a;
+	else return 8+a;
+}
 
 int main()
 {
+	int n = 0;
+	string str;
+	cin >> str;
+	for (int i = 0; i < str.size(); i++)
+	{
+		n += match(str.at(i));
+	}
+	cout << n;
 }
 
+// solve

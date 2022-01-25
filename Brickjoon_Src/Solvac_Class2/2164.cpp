dee44e8b98c0cb6c3159aa1cb4ccbffc803c9cd9
naixt1478 <qnqn60360@gmail.com>
2022-01-25 14:36:33 +0900

    2164.cpp solved &&
    Src to solvac_class2

---
+++ Brickjoon_Src/Solvac_Class2/2164.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	queue<int> q;
+	for (int i = 1; i <= n; i++)
+	{
+		q.push(i);
+	}
+	while (q.size() > 1)
+	{
+		q.pop();
+		q.push(q.front());
+		q.pop();
+	}
+	cout << q.front();
+}
+
+// solve

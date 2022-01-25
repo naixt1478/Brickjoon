ad374eac0807eab8c8d9e4078b3db63dea684783
naixt1478 <qnqn60360@gmail.com>
2022-01-25 14:32:58 +0900

    C71/A.cpp accepted

---
+++ Codeforces_Src/Div2/C4/A.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n % 2 != 0 || n==2)
+    {
+        cout << "NO";
+    }
+    else
+    {
+        cout << "YES";
+    }
+}
+
+// accepted

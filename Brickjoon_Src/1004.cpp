b0243ec4668a5b61e03e3501ebb9fe054fc0847b
naixt1478 <qnqn60360@gmail.com>
2022-08-02 16:38:36 +0900

    Auto stash before merge of "test" and "origin/test"

--- Brickjoon_Src/1004.cpp
+++ Brickjoon_Src/1004.cpp
@@ -1,26 +1,21 @@
 #include "bits/stdc++.h"
 #define __fastios ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr)
 using namespace std;
-namespace rng = ranges;
-
-int mp1(int a)
-{
-	return a * a;
-}
 
 void s1()
 {
-	int cx,cy,r,T2,x1, y1, x2, y2,i1{};
+	int x1, y1, x2, y2, T2, cx, cy, r, i1{};
 	cin >> x1 >> y1 >> x2 >> y2;
 	cin >> T2;
 	while (T2--)
 	{
 		cin >> cx >> cy >> r;
-		if(
-			(cx-r <= x1 && x1 <= cx+r && cy-r <= y1 && y1 <= cy+r) || 
-			(cx - r <= x2 && x2 <= cx + r && cy - r <= y2 && y2 <= cy + r))
+		if (cx - r < x1 && x1 < cx + r && cy - r < y1 && y1 < cy + r || cx - r < x2 && x2 < cx + r && cy - r < y2 && y2 < cy + r)
 		{
-			i1++;
+			if (!(cx - r < x1 && x1 < cx + r && cy - r < y1 && y1 < cy + r && cx - r < x2 && x2 < cx + r && cy - r < y2 && y2 < cy + r))
+			{
+				i1++;
+			}
 		}
 	}
 	cout << i1 << '\n';
@@ -31,7 +26,7 @@ int main()
 	__fastios;
 	int T;
 	cin >> T;
-	while(T--)
+	while (T--)
 	{
 		s1();
 	}
---
+++ Brickjoon_Src/Silver3/1406.cpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+#define __fastios ios::sync_with_stdio(0),cin.tie(nullptr),cout.tie(nullptr)
+using namespace std;
+namespace rng = ranges;
+
+int main()
+{
+	__fastios;
+}

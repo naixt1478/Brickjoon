47cd0c87ca7fed28304c09b975786bb5ebaa2346
naixt1478 <qnqn60360@gmail.com>
2022-11-18 23:34:45 +0900

    change build process-2

--- Brickjoon_Src/build.ps1
+++ Brickjoon_Src/build.ps1
@@ -1,20 +1,16 @@
+Remove-Item -Path ".\CMakeLists.txt" -Force
+New-Item -Path . -Name "CMakeLists.txt" -ItemType "file" -Force
+Add-Content -Path ".\CMakeLists.txt" -Value "#project name 
+project(brickjoon_source)
+
+"
+
 $Sources1 = Get-ChildItem -Path "." -File
 foreach ($Source1 in $Sources1)
 {
     if($Source1.Extension -eq ".cc" -or $Source1.Extension -eq ".cpp")
     {
-        New-Item -Path . -Name $Source1.BaseName -ItemType "Directory"
-        New-Item -Path $Source1.BaseName -Name "CMakeLists.txt" -ItemType "file"
-        $Path1 = -join($Source1.BaseName,"\CMakeLists.txt")
-        $Value3 = -join("boj_",$Source1.BaseName) 
-        $s1 = -join("project(",$Value3,")")
-        $s2 = -join("add_subdirectory(",$Source1.BaseName,")")
-        Add-Content -Path $Path1 -Value "#project name"
-        Add-Content -Path $Path1 -Value $s1
-        Add-Content -Path ".\CMakeLists.txt" -Value $s2
-        Move-Item -Path $Source1 -Destination $Source1.BaseName
-        $value1 = "add_executable(baekjoon_"
-        $Value2 = -join ($value1, $Source1.BaseName, " ", $Source1.Name, ")")
-        Add-Content -Path $Path1 -Value $Value2
+        $s1 = -join("add_executable(","baekjoon_",$Source1.BaseName," ",$Source1.Name,")")
+        Add-Content -Path ".\CMakeLists.txt" -Value $s1
     }
 }

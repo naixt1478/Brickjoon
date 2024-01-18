403825ef884986538dd9808f371830110c6ae127
naixt1478 <qnqn60360@gmail.com>
2024-01-19 02:20:33 +0900

    solving 7785

--- Brickjoon_Src/7785.c
+++ Brickjoon_Src/7785.c
@@ -1,12 +1,15 @@
+#define min(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 typedef long long int i64_t;
 typedef unsigned long long int ui64_t;
+typedef unsigned int ui32_t;
 
 typedef int data_t;
-
+typedef char* sort_t;
 typedef struct node
 {
     char key[21];
@@ -30,48 +33,86 @@ typedef struct htable
     int iter_len;
 } htable;
 
-int compare(const void* a, const void* b);
+void merge_sort(sort_t* arr, int len, int slen, int (*comp)(void*,void*));
+void merge(sort_t* arr, sort_t* warr, int l1, int r1, int e1, int (*comp)(void*,void*));
+int comp(void* a, void* b);
+
 node* init_node(char* key, data_t value);
 htable init_htable(int bsize);
-void free_htable(htable ht1);
+void free_htable(htable* ht1);
 
 int match_key(void* a, void* b);
-void insert_htable(htable ht1, char* key, data_t value, int bsize);
-void remove_htable(htable ht1, char* key, int bsize);
-int search_htable(htable ht1, char* key, int bsize);
-node* index_htable(htable ht1, char* key, int bsize);
+void insert_htable(htable* ht1, char* key, data_t value, int bsize);
+void remove_htable(htable* ht1, char* key, int bsize);
+int search_htable(htable* ht1, char* key, int bsize);
+node* index_htable(htable* ht1, char* key, int bsize);
 
-ui64_t make_key_djb2(char* key,int bsize);
+ui32_t make_key(const char* key,int bsize);
 
 int main(void)
 {
     int n;
     char c1[20],c2[20];
     scanf("%d", &n);
-    htable ht1 = init_htable(n);
+    htable ht1 = init_htable(n*2);
     for(int i = 0; i < n; i++)
     {
         scanf("%s %s", c1, c2);
-        if(strcmp(c2, "enter") == 0) index_htable(ht1,c1,n)->data = 1;
-        else remove_htable(ht1, c1, n);
+        if(strcmp(c2, "enter") == 0) insert_htable(&ht1, c1, 1, n*2);
+        else remove_htable(&ht1, c1, n*2);
     }
+    node* iter = ht1.iter_front;
     char** arr1 = (char**)calloc(ht1.iter_len, sizeof(char*));
     for(int i = 0; i < ht1.iter_len; i++)
     {
-        node* iter = ht1.iter_front;
-        arr1[i] = (char*)calloc(10, sizeof(char));
-        strcpy(arr1[i], iter->key);
+        arr1[i] = iter->key;
         iter = iter->iter_next;
     }
-    if(ht1.iter_len != 0) qsort(arr1, ht1.iter_len, sizeof(arr1[0]), compare);
+    if(ht1.iter_len != 0) merge_sort(arr1,ht1.iter_len,21,comp);
     for(int i = 0; i < ht1.iter_len; i++)
         printf("%s\n", arr1[i]);
-    free_htable(ht1);
+    free_htable(&ht1);
 }
 
-int compare(const void* a, const void* b)
+int comp(void* a, void* b)
 {
-    return -strcmp(*(char**)a, *(char**)b); 
+    if(strcmp((sort_t)a, (sort_t)b) <= 0) return 1;
+    else return 0;
+}
+
+void merge_sort(sort_t* arr, int len, int slen, int (*comp)(void*,void*))
+{  
+    sort_t* warr = (sort_t*)malloc(len*sizeof(sort_t));
+    //for(int i = 0; i < len; i++) warr[i] = (sort_t)calloc(slen, sizeof(char));
+    for(int i = 1; i < len; i*=2)
+    {
+        for(int j = 0; j < len; j += 2 * i)
+        {
+            merge(arr, warr, j, min(j+i, len), min(j+i*2,len), comp);
+        }
+        memcpy(arr, warr, len*sizeof(sort_t));
+    }
+    //for(int i = 0; i < len; i++) free(warr[i]);
+    free(warr);
+}
+
+void merge(sort_t* arr, sort_t* warr, int l1, int r1, int e1, int (*comp)(void*,void*))
+{
+    int i = l1;
+    int j = r1;
+    for(int k = l1; k < e1; k++)
+    {
+        if(i < r1 && (j >= e1 || (*comp)((void*)&arr[i], (void*)&arr[j])))
+        {
+            warr[k] = arr[i];
+            i++;
+        }
+        else
+        {
+            warr[k] = arr[j];
+            j++;
+        }
+    }
 }
 
 node* init_node(char* key, data_t value)
@@ -98,9 +139,9 @@ htable init_htable(int bsize)
     return ht1;
 }
 
-void free_htable(htable ht1)
+void free_htable(htable* ht1)
 {
-    node* iter_node1 = ht1.iter_front;
+    node* iter_node1 = ht1->iter_front;
     node* iter_node2 = NULL;
     while(iter_node1 != NULL)
     {
@@ -109,15 +150,14 @@ void free_htable(htable ht1)
         iter_node2 = NULL;
         iter_node1 = iter_node1->iter_next;
     }
-    free(ht1.hlist);
+    free(ht1->hlist);
 }
 
-node* index_htable(htable ht1, char* key, int bsize)
+node* index_htable(htable* ht1, char* key, int bsize)
 {
-    printf("now here1\n");
     if(search_htable(ht1, key, bsize) == 0) insert_htable(ht1, key, 0, bsize);
-    hlist hl1 = ht1.hlist[make_key_djb2(key, bsize)];
-    node* iter_node = hl1.root;
+    hlist* hl1 = &ht1->hlist[make_key(key, bsize)];
+    node* iter_node = hl1->root;
     while(iter_node != NULL)
     {
         if(match_key(iter_node->key, key))
@@ -127,59 +167,81 @@ node* index_htable(htable ht1, char* key, int bsize)
     return NULL;
 }
 
-void insert_htable(htable ht1, char* key, data_t value, int bsize)
+void insert_htable(htable* ht1, char* key, data_t value, int bsize)
 {
-    printf("now here2\n");
-    hlist* hl1 = &ht1.hlist[make_key_djb2(key, bsize)];
+    hlist* hl1 = &ht1->hlist[make_key(key, bsize)];
     node* inode1 = init_node(key, value);
     if(hl1->hlist_len != 0)
     {
         inode1->next = hl1->root;
     }
+    hl1->root = inode1;
+    if(ht1->iter_len > 0)
+    {
+        node* tmp1 = ht1->iter_back;
+        ht1->iter_back = inode1;
+        tmp1->iter_next = ht1->iter_back;
+        ht1->iter_back->iter_prev = tmp1;
+    }
     else
     {
-        hl1->root = inode1;
+        ht1->iter_front = inode1;
+        ht1->iter_back = inode1;
     }
-    if(ht1.iter_len != 0)
-    {
-        ht1.iter_back->iter_next = inode1;
-        inode1->iter_prev = ht1.iter_back;
-    }
-    else
-        ht1.iter_front = inode1;
-    ht1.iter_back = inode1;
     hl1->hlist_len += 1;
-    ht1.iter_len += 1;
+    ht1->iter_len += 1;
+    node* tmp2 = ht1->iter_front;
+    while(tmp2 != NULL)
+    {
+        tmp2 = tmp2->iter_next;
+    }
 }
-void remove_htable(htable ht1, char* key, int bsize)
+void remove_htable(htable* ht1, char* key, int bsize)
 {
-    printf("now here4\n");
-    hlist hl1 = ht1.hlist[make_key_djb2(key, bsize)];
+    hlist* hl1 = &ht1->hlist[make_key(key, bsize)];
     node* iter_pnode = NULL;
-    node* iter_node = hl1.root;
-    while(hl1.hlist_len != 0 && iter_node != NULL)
+    node* iter_node = hl1->root;
+    while(hl1->hlist_len != 0 && iter_node != NULL)
     {
         if(match_key(iter_node->key,key))
         {
-            hl1.hlist_len -= 1;
-            ht1.iter_len -= 1;
-            node* t1 = iter_node->next;
-            if(iter_node->iter_prev != NULL) iter_node->iter_prev->iter_next = iter_node->iter_next;
-            if(iter_node->iter_next != NULL) iter_node->iter_next->iter_prev = iter_node->iter_prev;
-            if(iter_pnode != NULL) iter_pnode->next = t1;
-            else hl1.root = t1;
+            hl1->hlist_len -= 1;
+            ht1->iter_len -= 1;
+            if(iter_node->iter_prev == NULL && iter_node->iter_next == NULL)
+            { 
+                ht1->iter_front = NULL;
+                ht1->iter_back = NULL;
+            }
+            else if(iter_node->iter_next != NULL && iter_node->iter_prev == NULL) 
+            {
+                ht1->iter_front = iter_node->iter_next;
+                iter_node->iter_next->iter_prev = NULL;
+            }
+            else if(iter_node->iter_next == NULL && iter_node->iter_prev != NULL)
+            {
+                ht1->iter_back = iter_node->iter_prev;
+                iter_node->iter_prev->iter_next = NULL;
+            }
+            else
+            {
+                iter_node->iter_prev->iter_next = iter_node->iter_next;
+                iter_node->iter_next->iter_prev = iter_node->iter_prev;
+            }
+            if(iter_pnode == NULL)
+                hl1->root = iter_node->next;
+            else
+                iter_pnode->next = iter_node->next;
             free(iter_node);
         }
         iter_pnode = iter_node;
         iter_node = iter_node->next;
     }
 }
-int search_htable(htable ht1, char* key, int bsize)
+int search_htable(htable* ht1, char* key, int bsize)
 {
-    printf("now here3\n");
-    hlist hl1 = ht1.hlist[make_key_djb2(key, bsize)];
-    node* iter_node = hl1.root;
-    while(hl1.hlist_len != 0 && iter_node != NULL)
+    hlist* hl1 = &ht1->hlist[make_key(key, bsize)];
+    node* iter_node = hl1->root;
+    while(hl1->hlist_len != 0 && iter_node != NULL)
     {
         if(match_key(iter_node->key, key))
             return 1;
@@ -193,11 +255,19 @@ int match_key(void* a, void* b)
     return ((strcmp(a,b) == 0) ? 1 : 0);
 }
 
-ui64_t make_key_djb2(char* key, int bsize)
+ui32_t make_key(const char* key, int bsize)
 {
-    ui64_t h = 5381;
-    int c;
-    while ((c = *key++))
-        h = ((h << 5) + h) + c; /* hash * 33 + c */
-    return h % bsize;
+    // one_at_a_time
+    size_t i = 0;
+    ui64_t length = strlen(key);
+    ui32_t hash = 0;
+    while (i != length) {
+    hash += (unsigned char)key[i++];
+    hash += hash << 10;
+    hash ^= hash >> 6;
+    }
+    hash += hash << 3;
+    hash ^= hash >> 11;
+    hash += hash << 15;
+    return hash % bsize;
 }
--- Brickjoon_Src/Implementation/sort.c
+++ Brickjoon_Src/Implementation/sort.c
@@ -4,8 +4,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void merge_sort(int*, int, int);
-void merge(int*,int*,int,int,int,int);
+typedef int sort_t;
+
+void merge_sort(sort_t* arr, int len, int comp);
+void merge(sort_t* arr, sort_t* warr,int l1,int r1,int e1,int comp);
+int comp(void* a, void* b);
 
 int main(void)
 {
@@ -16,27 +19,33 @@ int main(void)
 // comp 0 : 오름차순
 // len : 배열크기
 // arr : 배열포인터(배열 주소)
-void merge_sort(int* arr, int len, int comp)
+int comp(void* a, void* b)
+{
+    if(*(int*)a <= *(int*)b) return 1;
+    else return 0;
+}
+
+void merge_sort(sort_t* arr, int len, int comp)
 {  
-    int* warr = (int*)malloc(len*sizeof(int));
+    sort_t* warr = (sort_t*)malloc(len*sizeof(sort_t));
     for(int i = 1; i < len; i*=2)
     {
         for(int j = 0; j < len; j += 2 * i)
         {
             merge(arr, warr, j, min(j+i, len), min(j+i*2,len), comp);
         }
-        memcpy(arr, warr, len*sizeof(int));
+        memcpy(arr, warr, len*sizeof(sort_t));
     }
     free(warr);
 }
 
-void merge(int* arr, int* warr, int l1, int r1, int e1, int comp)
+void merge(sort_t* arr, sort_t* warr, int l1, int r1, int e1, int (*comp)(void*,void*))
 {
     int i = l1;
     int j = r1;
     for(int k = l1; k < e1; k++)
     {
-        if(i < r1 && (j >= e1 || (arr[i] <= arr[j]) != comp))
+        if(i < r1 && (j >= e1 || (*comp)(&arr[i], &arr[j])))
         {
             warr[k] = arr[i];
             i++;
